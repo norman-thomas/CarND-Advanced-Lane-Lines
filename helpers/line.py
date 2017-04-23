ddb1e00266c57699503602039f5695461cf620f8
Norman Thomas <news@hiroki.de>
2017-04-23 16:35:51 +0200

    lane detection partially working

--- helpers/line.py
+++ helpers/line.py
@@ -70,18 +70,26 @@ class LaneSearch:
         else:
             return self._search_dumb(draw=draw)
 
-    def _draw_rect(corners):
-        pass
-
-    def _draw_line(points):
-        pass
-
     def _search_dumb(self, draw=False):
         def _is_tolerable(val, previous_val, skipped):
-            return abs(val - previous_val) <= (1+skipped) * self._window_width//2
+            return abs(val - previous_val) <= _tolerance(skipped)
+
+        def _tolerance(skipped):
+            return (1+skipped) * self._window_width//2
+
+        def _draw_rectangle(x, y_from, y_to, found=True, delta=None):
+            delta = delta if delta is not None else self._window_width//2
+            lim_left = 0 if x < self.image.shape[1]//2 else self.image.shape[1]//2
+            lim_right = self.image.shape[1]//2 if x < self.image.shape[1]//2 else self.image.shape[1]
+            p1x = max(lim_left, x - delta)
+            p2x = min(x + delta, lim_right)
+            color = (255, 0, 0) if not found else (0, 255, 0)
+            cv2.rectangle(self._draw_image, (p1x, y_from), (p2x, y_to), color=color, thickness=6)
 
         height = self.image.shape[0]
-        hist = histogram(self.image, from_=int(height * (1 - 1/self._initial_fraction)))
+        from_ = height - (height // self._initial_fraction)
+        to_ = height
+        hist = histogram(self.image, from_=from_)
         left, right = find_maximums(hist)
         windows = []
         left_centroids = [(left, height)]
@@ -89,27 +97,37 @@ class LaneSearch:
 
         previous_left = left
         previous_right = right
+        if draw:
+            _draw_rectangle(previous_left, height - self.window_height, to_)
+            _draw_rectangle(previous_right, height - self.window_height, to_)
 
         left_skipped = 0
         right_skipped = 0
         for row in range(1, self.window_count):
-            from_ = height - (row+1) * self.window_height
-            to_ = height - row * self.window_height
             section = self.image[from_:to_]
             hist = histogram(section)
             left, right = find_maximums(hist)
+            center_height = int(height - (row + 0.5) * self.window_height)
             if _is_tolerable(left, previous_left, left_skipped):
                 left_skipped = 0
                 previous_left = left
-                left_centroids += [(left, height - row * self.window_height)]
+                left_centroids += [(left, center_height)]
             else:
                 left_skipped += 1
             if _is_tolerable(right, previous_right, right_skipped):
                 right_skipped = 0
                 previous_right = right
-                right_centroids += [(right, height - row * self.window_height)]
+                right_centroids += [(right, center_height)]
             else:
                 right_skipped += 1
+
+            from_ = height - (row+1) * self.window_height
+            to_ = height - row * self.window_height
+            if draw:
+                delta = _tolerance(left_skipped)
+                _draw_rectangle(previous_left, from_, to_, left_skipped == 0, delta=delta)
+                delta = _tolerance(right_skipped)
+                _draw_rectangle(previous_right, from_, to_, right_skipped == 0, delta=delta)
 
         left_centroids = np.array(left_centroids)
         right_centroids = np.array(right_centroids)
